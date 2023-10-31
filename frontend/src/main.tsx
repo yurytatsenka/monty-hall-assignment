9f94d6adb451a6eea428f61ced4424e1c627d8aa
Yury Tatsenka <forthepost@gmail.com>
2023-10-31 12:27:30 +0300

    TR-01: add tracker

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Content } from './components/Content'
 import './index.css'
+import { Tracker } from './components/Tracker'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <div className='flex flex-col items-center h-max h-screen pb-64 pt-10 w-full min-w-min'>
-      <div className="flex text-center text-gray-500 text-4xl pb-12 px-12">Monty Hall Simulation</div>
-      <Content />
+      <div className="flex text-center text-gray-500 text-4xl pb-12 px-12">Time Tracker</div>
+      <Tracker />
     </div>
   </React.StrictMode>
 )
